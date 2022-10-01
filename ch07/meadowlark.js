014c51dcfb28f9834b217762e61f00edac749578
liu <839292885@qq.com>
2022-10-01 10:57:25 +0900

    ch08

--- ch07/meadowlark.js
+++ ch07/meadowlark.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+var express = require('express'),
+    fortune = require('./lib/fortune.js'),
+    formidable = require('formidable'),
+    jqupload = require('jquery-file-upload-middleware');
+
 var app = express();
 
 var handlebars = require('express-handlebars').create({ 
@@ -18,6 +21,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
@@ -60,6 +64,17 @@ app.use(function(req, res, next){
  	next();
 });
 
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function(){
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function(){
+            return '/uploads/' + now;
+        },
+    })(req, res, next);
+});
 
 app.get('/', function(req, res){
 	res.render('home');
@@ -105,6 +120,63 @@ app.get('/data/nursery-rhyme', function(req, res){
 	});
 });
 
+app.get('/thank-you',function(req,res){
+	res.render('thank-you');
+});
+
+app.get('/newsletter',function(req,res){
+	res.render('newsletter',{csrf:'CSRF token goes here'});
+});
+
+app.post('/process',function(req,res){
+	console.log('Form (from queryingstring):' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you');
+	//res.render('thank-you');
+});
+
+app.get('/newsletter_ajax',function(req,res){
+	res.render('newsletter_ajax',{csrf:'CSRF token goes here'});
+});
+
+app.post('/process_ajax',function(req,res){
+	console.log(req.get('Accept'));  
+	console.log(req.accepts('json','html'));
+
+	if(req.xhr || req.accepts('json,html')==='json'){
+		res.send({success : true});
+	} else{
+		res.redirect(303, '/thank-you');
+	}
+});
+
+app.get('/contest/vacation-photo',function(req,res){
+	var now = new Date();
+	res.render('contest/vacation-photo',{
+		year:now.getFullYear(),month:now.getMonth()
+	});
+});
+
+app.get('/contest/jq_vacation-photo',function(req,res){
+	var now = new Date();
+	res.render('contest/jq_vacation-photo',{
+		year:now.getFullYear(),month:now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month',function(req,res){
+	var form = new formidable.IncomingForm();
+	form.parse(req,function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+        	console.log('received fields:');
+        	console.log(fields);
+        	console.log('received files:');
+        	console.log(files);
+        	res.redirect(303, '/thank-you');
+	});
+});
 app.use(function(req, res, next){
 	res.status(404);
 	res.send('404');
